d74ab051cc1115796ef9354aeaef218e883f3e27
Alya Zagatskaya <alya.zagacki@gmail.com>
2022-08-21 16:54:17 +0300

    refactored

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
 window.onload = function () {
   let blocks = document.getElementsByClassName("color__block");
+  let color = null;
   for (const elem of blocks) {
-    setRandomColor(elem, getRandomColor());
+    color = getRandomColor();
+    setRandomColor(elem, color);
   }
 };
 
 function getRandomColor() {
-  return Math.floor(Math.random() * 16777215).toString(16);
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-function setRandomColor(elem, color){
-  elem.style.backgroundColor = "#" + color;
+function setRandomColor(elem, color) {
+  elem.style.backgroundColor = color;
 }
 
-function changeColor(elem){
+function changeColor(elem) {
   const currentColor = elem.style.backgroundColor;
   const color = getRandomColor();
-  elem.style.backgroundColor = (color !== currentColor) ? color : getRandomColor();
+  elem.style.backgroundColor =
+    color !== currentColor ? color : getRandomColor();
 }
 
 function rightPic() {
